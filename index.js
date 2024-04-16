fbcbb7302eed69526bfa35f3996d2df5dedad223
Sunioatm <104454675+Sunioatm@users.noreply.github.com>
2024-04-16 22:23:00 +0700

    fetch json from github

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const express = require('express')
 const line = require('@line/bot-sdk')
+const axios = require('axios');
+
 require('dotenv').config()
 
 
 const app = express()
+
+const jsonFileUrl = 'https://raw.githubusercontent.com/Sunioatm/Predicted-JSON/main/test.json';
+// define jsonData
+let jsonData = null;
+axios.get(jsonFileUrl)
+  .then(response => {
+    jsonData = response.data;
+    // Use the jsonData object here
+    console.log(jsonData);
+  })
+  .catch(error => {
+    console.error('Error reading JSON file:', error);
+  });
+
 
 const config = {
   channelAccessToken : process.env.channelAccessToken,
@@ -30,25 +46,25 @@ function handleEvents(event){
   if ((event.message.text).toLowerCase().includes('btc')) {
     return client.replyMessage(event.replyToken, {
       type: 'text',
-      text: 'ทำนายราคาวันที่ 1 : $\nทำนายราคาวันที่ 2 $\nทำนายราคาวันที่ 3 $'
+      text : `ทำนายราคาวันที่ 1 : ${jsonData.BTC/USD_d[0]}\nทำนายราคาวันที่ 2 : ${jsonData.BTC/USD_d[1]}\nทำนายราคาวันที่ 3 : ${jsonData.BTC/USD_d[2]}`
     });
   }
   if ((event.message.text).toLowerCase().includes('nasdaq')) {
     return client.replyMessage(event.replyToken, {
       type: 'text',
-      text: 'ทำนายราคาวันที่ 1 : $\nทำนายราคาวันที่ 2 $\nทำนายราคาวันที่ 3 $'
+      text : `ทำนายราคาวันที่ 1 : ${jsonData.NASDAQ/USD_d[0]}\nทำนายราคาวันที่ 2 : ${jsonData.NASDAQ/USD_d[1]}\nทำนายราคาวันที่ 3 : ${jsonData.NASDAQ/USD_d[2]}`
     });
   }
-  if ((event.message.text).toLowerCase().includes('set')){
+  if ((event.message.text).toLowerCase().includes('set')) {
     return client.replyMessage(event.replyToken, {
       type: 'text',
-      text: 'ทำนายราคาวันที่ 1 : $\nทำนายราคาวันที่ 2 $\nทำนายราคาวันที่ 3 $'
+      text : `ทำนายราคาวันที่ 1 : ${jsonData.SET/THB_d[0]}\nทำนายราคาวันที่ 2 : ${jsonData.SET/THB_d[1]}\nทำนายราคาวันที่ 3 : ${jsonData.SET/THB_d[2]}`
     });
   }
   if ((event.message.text).toLowerCase().includes('nvda')) {
     return client.replyMessage(event.replyToken, {
       type: 'text',
-      text: 'ทำนายราคาวันที่ 1 : $\nทำนายราคาวันที่ 2 $\nทำนายราคาวันที่ 3 $'
+      text : `ทำนายราคาวันที่ 1 : ${jsonData.NVDA/USD_d[0]}\nทำนายราคาวันที่ 2 : ${jsonData.NVDA/USD_d[1]}\nทำนายราคาวันที่ 3 : ${jsonData.NVDA/USD_d[2]}`
     });
   }
   return client.replyMessage(event.replyToken, {
