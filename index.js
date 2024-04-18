fd4cab75e953a4306898dc46773fded811a9a842
Sunioatm <104454675+Sunioatm@users.noreply.github.com>
2024-04-18 21:40:28 +0700

    fixed

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ require('dotenv').config()
 
 const app = express()
 
+const jsonFileUrl = 'https://raw.githubusercontent.com/Sunioatm/Predicted-JSON/main/predicted.json';
 // JSON Data Cache
 let jsonData = null;
 
